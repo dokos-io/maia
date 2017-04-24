3089facdcfc27b42868454378024bd0ecef86d8e
Charles-Henri Decultot <chdecultot@dokos.io>
2017-04-24 13:38:19 +0000

    Bump to v0.4.0 and patient rename patch

---
+++ maia/patches/v0_4/rename_patient_doctype.py
@@ -0,0 +1,7 @@
+import frappe
+
+def execute():
+    if frappe.db.table_exists("Patient") and not frappe.db.table_exists("Patient Record"):
+    frappe.rename_doc("DocType", "Patient", "Patient Record")
+
+    frappe.delete_doc('DocType', 'Patient')
