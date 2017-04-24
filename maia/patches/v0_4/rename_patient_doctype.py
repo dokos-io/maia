eb61d8269e9b81725df9548627c870590cedca17
Charles-Henri Decultot <chdecultot@dokos.io>
2017-04-24 14:42:04 +0000

    Addition to the renaming patch

--- maia/patches/v0_4/rename_patient_doctype.py
+++ maia/patches/v0_4/rename_patient_doctype.py
@@ -2,6 +2,7 @@ import frappe
 
 def execute():
     if frappe.db.table_exists("Patient") and not frappe.db.table_exists("Patient Record"):
+        frappe.db.sql(""" delete from `tabDesktop Icon` where link like 'List/Patient' """)
         frappe.rename_doc("DocType", "Patient", "Patient Record")
 
         frappe.delete_doc('DocType', 'Patient')
