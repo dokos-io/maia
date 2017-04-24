fe9fac7380fefec6c0a69c84b7d347748ec5e4dd
Charles-Henri Decultot <chdecultot@dokos.io>
2017-04-24 13:55:18 +0000

    Patch indent correction

--- maia/patches/v0_4/rename_patient_doctype.py
+++ maia/patches/v0_4/rename_patient_doctype.py
@@ -2,6 +2,6 @@ import frappe
 
 def execute():
     if frappe.db.table_exists("Patient") and not frappe.db.table_exists("Patient Record"):
-    frappe.rename_doc("DocType", "Patient", "Patient Record")
+        frappe.rename_doc("DocType", "Patient", "Patient Record")
 
-    frappe.delete_doc('DocType', 'Patient')
+        frappe.delete_doc('DocType', 'Patient')
