79dd5ee41438ab898b1735165855f440cc874b1a
Charles-Henri Decultot <chdecultot@dokos.io>
2017-07-05 14:49:48 +0000

    Email reminder improvement

--- maia/maia/doctype/midwife_appointment/midwife_appointment.py
+++ maia/maia/doctype/midwife_appointment/midwife_appointment.py
@@ -10,8 +10,9 @@ import datetime
 import time
 from dateutil.relativedelta import relativedelta
 from frappe import _
-from frappe.utils import getdate, get_datetime, get_datetime_str, formatdate
+from frappe.utils import getdate, get_datetime, get_datetime_str, formatdate, now_datetime
 from frappe.contacts.address_and_contact import load_address_and_contact
+from frappe.email.doctype.standard_reply.standard_reply import get_standard_reply
 
 class MidwifeAppointment(Document):       
         def on_submit(self):
@@ -20,19 +21,38 @@ class MidwifeAppointment(Document):
                         self.send_reminder()
 
         def send_reminder(self):
-                patient_first_name = frappe.db.get_value("Patient Record",self.patient_record,"patient_first_name")
                 patient_email = self.email
-                appointment_date = formatdate(get_datetime_str(self.start_dt), "dd/MM/yyyy")
                 sending_date = get_datetime(self.date) + relativedelta(days=-1)
 
-                start_time = get_datetime(self.start_dt).strftime("%H:%M")
+                if self.standard_reply != None:
+                        args = {
+                                "patient_record": self.patient_record,
+                                "patient_first_name": frappe.db.get_value("Patient Record",self.patient_record,"patient_first_name"),
+                                "patient_last_name": frappe.db.get_value("Patient Record",self.patient_record,"patient_last_name"),
+                                "practitioner": self.practitioner,
+                                "appointment_type": self.appointment_type,
+                                "patient_name": self.patient_name,
+                                "date": formatdate(get_datetime_str(self.start_dt), "dd/MM/yyyy"),
+                                "start_time": get_datetime(self.start_dt).strftime("%H:%M"),
+                                "standard_reply": self.standard_reply,
+                                "duration": self.duration
+                        }
+                        reply = get_standard_reply(self.standard_reply, args)
+                        
+                        subject = reply["subject"]
+                        message = reply["message"]
+                else:
+                        patient_first_name = frappe.db.get_value("Patient Record",self.patient_record,"patient_first_name")
+                        appointment_date = formatdate(get_datetime_str(self.start_dt), "dd/MM/yyyy")
+                        start_time = get_datetime(self.start_dt).strftime("%H:%M")
                 
-                subject = _("""N'oubliez pas votre rendez-vous avec {0}, prévu le {1} à {2}""".format(self.practitioner, appointment_date, start_time))
-                content = _("""Bonjour {0}, <br><br>Votre rendez-vous est toujours prévu le {1}, à {2}. <br><br>Si vous avez un empêchement, veuillez me l'indiquer au plus vite par retour de mail.<br><br>Merci beaucoup.<br><br>{3}""".format(patient_first_name, appointment_date, start_time, self.practitioner))
+                        subject = _("""N'oubliez pas votre rendez-vous avec {0}, prévu le {1} à {2}""".format(self.practitioner, appointment_date, start_time))
+                        message = _("""Bonjour {0}, <br><br>Votre rendez-vous est toujours prévu le {1}, à {2}. <br><br>Si vous avez un empêchement, veuillez me l'indiquer au plus vite par retour de mail.<br><br>Merci beaucoup.<br><br>{3}""".format(patient_first_name, appointment_date, start_time, self.practitioner))
 
-
-                frappe.sendmail(patient_email, subject=subject, content=content, send_after=sending_date)
-                
+                if sending_date > now_datetime():
+                        frappe.sendmail(patient_email, subject=subject, content=message, send_after=sending_date)
+                else:
+                        frappe.sendmail(patient_email, subject=subject, content=message)
 
 @frappe.whitelist()
 def update_status(appointmentId, status):
