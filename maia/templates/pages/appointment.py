b2a4d23e5fdc2fa48f5e0402938d350acf9eb092
Charles-Henri Decultot <chdecultot@dokos.io>
2017-07-19 13:08:37 +0000

    Scheduler refactor, App Type improvement and change_log for v1.2.3

--- maia/templates/pages/appointment.py
+++ maia/templates/pages/appointment.py
@@ -8,6 +8,7 @@ from frappe import _
 import datetime
 from datetime import timedelta, date
 import calendar
+from maia.maia.scheduler import get_availability_from_schedule
 
 def get_context(context):
             context.appointment_type = frappe.get_list("Midwife Appointment Type", fields=['name'])
@@ -15,6 +16,8 @@ def get_context(context):
                     
 
 def daterange(start_date, end_date):
+            if start_date < now_datetime():
+                        start_date = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
             for n in range(int ((end_date - start_date).days)):
                         yield start_date + timedelta(n)
                                     
@@ -47,7 +50,7 @@ def submit_appointment(patient_record, practitioner, appointment_type, start, en
 
             start_date = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S').date()
             start_time = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S').time()
-            duration = frappe.get_value("Midwife Appointment Type", appointment_type, "duration")
+            app_type = frappe.get_doc("Midwife Appointment Type", appointment_type)
             
             appointment = frappe.get_doc({
                         "doctype": "Midwife Appointment",
@@ -58,7 +61,8 @@ def submit_appointment(patient_record, practitioner, appointment_type, start, en
                         "start_time": start_time,
                         "start_dt": start,
                         "end_dt": end,
-                        "duration": duration
+                        "duration": app_type.duration,
+                        "color": app_type.color
             }).insert()
 
             appointment.submit()
@@ -86,67 +90,3 @@ def check_availability(doctype, df, dt, dn, date, duration):
             if schedules:
                 availability.extend(get_availability_from_schedule(doctype, df, dn, schedules, date))            
     return availability
-
-def get_availability_from_schedule(doctype, df, dn, schedules, date):
-    data = []
-    for line in schedules:
-        duration = get_time(line["duration"])
-        scheduled_items = frappe.db.sql("""select start_dt, duration from `tab{0}` where (docstatus=0 or docstatus=1) and {1}='{2}' and start_dt between '{3}' and '{4}' order by start_dt""".format(doctype, df, dn, line["start"], line["end"]))
-
-        available_slot = find_available_slot(date, duration, line, scheduled_items)
-        data.append(available_slot)
-        
-    return data
-
-def find_available_slot(date, duration, line, scheduled_items):
-    
-    available_slots = []
-    current_schedule = []
-    if scheduled_items:
-        for scheduled_item in scheduled_items:
-
-            dur = datetime.timedelta(minutes = cint(scheduled_item[1]))
-            item_end = scheduled_item[0] + dur 
-            new_entry = (scheduled_item[0], item_end)
-            current_schedule.append(new_entry)
-
-        scheduled_items = current_schedule
-        slots = get_all_slots(line["start"], line["end"], line["duration"], scheduled_items)
-        
-        for slot in slots:
-                    available_slots.append(get_dict(slot[0], slot[1]))
-
-        return available_slots
-
-    else:
-        slots = get_all_slots(line["start"], line["end"], line["duration"])
-
-        for slot in slots:
-                    available_slots.append(get_dict(slot[0], slot[1]))
-
-        return available_slots
-
-def get_all_slots(day_start, day_end, time_delta, scheduled_items=None):
-    interval = int(time_delta.total_seconds() / 60)
-
-    if scheduled_items:
-                slots = sorted([(day_start, day_start)] + scheduled_items + [(day_end, day_end)])
-    else:
-                slots = sorted([(day_start, day_start)] + [(day_end, day_end)])
-
-    free_slots = []
-    for start, end in ((slots[i][1], slots[i+1][0]) for i in range(len(slots)-1)):
-            while start + timedelta(minutes=interval) <= end:
-                        free_slots.append([start, start + timedelta(minutes=interval)])
-                        start += timedelta(minutes=interval)
-    return free_slots
-
-def get_dict(start, end):
-    return {"start": start, "end": end, "all_day": 0, "id": start}
-
-def roundTime(dt, dateDelta=datetime.timedelta(minutes=5)):
-    roundTo = dateDelta.total_seconds()
-    seconds = (dt - dt.min).seconds            
-    rounding = (seconds+roundTo/2) // roundTo * roundTo
-
-    return dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond)
