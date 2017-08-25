30f3f5eb1162fd2e639ab4651f996c7b269da47d
Charles-Henri Decultot <chdecultot@dokos.io>
2017-08-25 15:52:11 +0000

    Patch correction + new Mammography doctype

--- maia/patches/v1_3/update_subject_field.py
+++ maia/patches/v1_3/update_subject_field.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-    frappe.db.sql("""UPDATE `tabMidwife Appointment` SET subject = patient_name """)
+	try:
+    		frappe.db.sql("""UPDATE `tabMidwife Appointment` SET subject = patient_name """)
+	except:
+		pass
