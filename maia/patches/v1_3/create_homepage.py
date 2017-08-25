9e2779ed17400ffb23017509e8113c2af3ebf0ef
Charles-Henri Decultot <chdecultot@dokos.io>
2017-08-25 13:12:35 +0000

    Homepage patch

---
+++ maia/patches/v1_3/create_homepage.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+    homepage = frappe.get_doc('Homepage', 'Homepage')
+    company = frappe.get_all('Company')
+    homepage.company = company[0].name
+    homepage.tag_line = company[0].name
+    homepage.description = "Connectez-vous pour prendre rendez-vous"
+    homepage.save()
