d1dff0bbfcaa229ad902f4545af30eda79d8072b
Charles-Henri Decultot <chdecultot@dokos.io>
2017-10-05 22:49:18 +0000

    Portal practitioner bug correction

--- maia/patches/v1_4/appointment_list.py
+++ maia/patches/v1_4/appointment_list.py
@@ -4,6 +4,14 @@ import frappe
 def execute():
     frappe.reload_doctype("Portal Settings")
 
+    items = frappe.get_all("Portal Menu Item",fields=['name', 'title', 'route', 'enabled'])
+
+    for item in items:
+        if item.route == "/appointment" or item.route == "/my-appointments":
+            pass
+        else:
+            frappe.db.set_value("Portal Menu Item", item.name, "enabled", 0)
+
     appointment = frappe.get_all("Portal Menu Item",filters={'route': '/my-appointments'})
 
     if appointment == []:
--- maia/templates/pages/appointment.py
+++ maia/templates/pages/appointment.py
@@ -13,8 +13,13 @@ from maia.maia.scheduler import get_availability_from_schedule
 
 def get_context(context):
             context.no_cache = 1
-            context.practitioner = frappe.get_list("Professional Information Card", filters={"allow_online_booking": 1}, fields=['name'])
-            context.practitioner.sort()
+            practitioners = frappe.get_list("Professional Information Card", filters={"allow_online_booking": 1}, fields=['name'])
+            if practitioners:
+                practitioners.sort()
+                context.practitioner = practitioners
+            else:
+                context.practitioner = []
+
             context.appointment_type = frappe.db.sql("""SELECT name from `tabMidwife Appointment Type` WHERE allow_online_booking=1 AND (practitioner='{0}' OR practitioner IS NULL)""".format(context.practitioner[0].name), as_dict=True)
 
 def daterange(start_date, end_date):
