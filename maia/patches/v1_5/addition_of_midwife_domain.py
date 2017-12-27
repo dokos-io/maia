566afbd6d564c7e6602147a5e6b5e870f65e4a75
Charles-Henri Decultot <chdecultot@dokos.io>
2017-12-27 17:50:29 +0000

    Patch correction + PI cleanup

--- maia/patches/v1_5/addition_of_midwife_domain.py
+++ maia/patches/v1_5/addition_of_midwife_domain.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
-from frappe.frappe.custom.doctype.customize_form.customize_form import reset_to_defaults
 
 
 def execute():
