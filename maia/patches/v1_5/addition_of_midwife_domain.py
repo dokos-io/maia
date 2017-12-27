020b92dc15592c63f60445cbca576171244e20aa
Charles-Henri Decultot <chdecultot@dokos.io>
2017-12-27 09:08:59 +0000

    Migration to v10

---
+++ maia/patches/v1_5/addition_of_midwife_domain.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import frappe
+from frappe.frappe.custom.doctype.customize_form.customize_form import reset_to_defaults
+
+
+def execute():
+	doc = frappe.new_doc("Domain")
+	doc.domain = "Sage Femme"
+	try:
+		doc.insert(ignore_permissions=True)
+		frappe.db.commit()
+	except Exception as e:
+		print(e)
+
+	# Reset company customization to default
+	frappe.db.sql("""delete from `tabProperty Setter` where doc_type=%s
+			and ifnull(field_name, '')!='naming_series'""", "Company")
+	frappe.clear_cache(doctype="Company")
