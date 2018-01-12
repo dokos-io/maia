96ec7a1fe9a7ed82a174d686e962ec2b641cffb1
Charles-Henri Decultot <chdecultot@dokos.io>
2018-01-12 12:35:41 +0000

    Consultation modification feature

--- maia/maia/doctype/early_postnatal_consultation/early_postnatal_consultation.py
+++ maia/maia/doctype/early_postnatal_consultation/early_postnatal_consultation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017, DOKOS and contributors
+# Copyright (c) 2018, DOKOS and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -8,10 +8,15 @@ from frappe.model.document import Document
 from maia.maia.invoicing import create_and_submit_invoice, get_customer_name, cancel_consultation_and_invoice
 
 class EarlyPostnatalConsultation(Document):
+	def before_insert(self):
+		remove_cancelled_invoice(self)
 
-        def on_submit(self):
-                get_customer_name(self)
-                create_and_submit_invoice(self)
+	def validate(self):
+		remove_cancelled_invoice(self)
 
-        def on_cancel(self):
-                cancel_consultation_and_invoice(self)
+	def on_submit(self):
+		get_customer_name(self)
+		create_and_submit_invoice(self)
+
+	def on_cancel(self):
+		cancel_consultation_and_invoice(self)
--- maia/maia/doctype/pregnancy_consultation/pregnancy_consultation.py
+++ maia/maia/doctype/pregnancy_consultation/pregnancy_consultation.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017, DOKOS and contributors
+# Copyright (c) 2018, DOKOS and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from maia.maia.invoicing import create_and_submit_invoice, get_customer_name, cancel_consultation_and_invoice
+from maia.maia.invoicing import create_and_submit_invoice, get_customer_name, cancel_consultation_and_invoice, remove_cancelled_invoice
 
 class PregnancyConsultation(Document):
+		def before_insert(self):
+			remove_cancelled_invoice(self)
 
-        def on_submit(self):
-                get_customer_name(self)
-                create_and_submit_invoice(self)
+		def validate(self):
+			remove_cancelled_invoice(self)
 
-        def on_cancel(self):
-                cancel_consultation_and_invoice(self)
+		def on_submit(self):
+			get_customer_name(self)
+			create_and_submit_invoice(self)
+
+		def on_cancel(self):
+			cancel_consultation_and_invoice(self)
