aa711ae5e503d73e67e5a169f7a815b7a3e010cf
Charles-Henri Decultot <chdecultot@dokos.io>
2018-03-01 10:55:56 +0000

    Bug Corrections

--- maia/maia/doctype/postnatal_consultation/postnatal_consultation.js
+++ maia/maia/doctype/postnatal_consultation/postnatal_consultation.js
@@ -15,6 +15,44 @@ maia.PostNatalConsultationController = frappe.ui.form.Controller.extend({
       }
     }
 
+  },
+	refresh: function(frm) {
+    if (!this.frm.doc.__islocal) {
+      this.frm.add_custom_button(__('Drug Prescription'), this.print_drug_prescription, __("Print Prescription"));
+      this.frm.add_custom_button(__('Lab Prescription'), this.print_lab_prescription, __("Print Prescription"));
+    }
+  },
+	print_drug_prescription: function(frm) {
+    var w = window.open(
+      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+        "&name=" + encodeURIComponent(cur_frm.doc.name) +
+        "&format=Drug Prescription" +
+        "&no_letterhead=0" +
+        "&_lang=fr")
+    );
+    if (!w) {
+      frappe.msgprint(__("Please enable pop-ups"));
+      return;
+    }
+  },
+
+  print_lab_prescription: function(frm) {
+    var w = window.open(
+      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+        "&name=" + encodeURIComponent(cur_frm.doc.name) +
+        "&format=Lab Prescription" +
+        "&no_letterhead=0" +
+        "&_lang=fr")
+    );
+    if (!w) {
+      frappe.msgprint(__("Please enable pop-ups"));
+      return;
+    }
+  },
+	set_default_print_format: function() {
+    this.frm.meta.default_print_format = "";
   }
 });
 $.extend(this.frm.cscript, new maia.PostNatalConsultationController({
