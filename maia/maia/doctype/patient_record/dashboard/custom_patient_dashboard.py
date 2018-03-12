236df9d698c29ff763fdfb29b9039382f1ef219e
Charles-Henri Decultot <chdecultot@dokos.io>
2018-03-12 15:44:09 +0000

    Memo bug correction

--- maia/maia/doctype/patient_record/dashboard/custom_patient_dashboard.py
+++ maia/maia/doctype/patient_record/dashboard/custom_patient_dashboard.py
@@ -211,7 +211,7 @@ def get_data(patient_record):
 	if generaldata:
 		data.append({'general': generaldata})
 
-	if pregnancydata and (pregnancydata['expected_term'] is not None or pregnancydata['beginning_of_pregnancy'] is not None):
+	if pregnancydata and (('expected_term' in pregnancydata and ['expected_term'] is not None) or ('beginning_of_pregnancy' in pregnancydata and pregnancydata['beginning_of_pregnancy'] is not None)):
 		data.append({'pregnancy': pregnancydata})
 
 	if deliverydata:
@@ -220,7 +220,7 @@ def get_data(patient_record):
 	if newborndata['firstchild'] or newborndata['secondchild'] or newborndata['thirdchild']:
 		data.append({'newborn': newborndata})
 
-	if labexamsdata and labexamsdata['exam_results'] is not None:
+	if labexamsdata and ('exam_results' in labexamsdata and labexamsdata['exam_results'] is not None):
 		data.append({'labexams': labexamsdata})
 
 	if perehabilitationdata:
