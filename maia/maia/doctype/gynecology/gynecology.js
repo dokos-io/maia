513286ff1aaa4603b22dfb24554ab0c51f12a931
Charles-Henri Decultot <chdecultot@dokos.io>
2018-03-19 15:24:28 +0000

    Addition of exam results templates

--- maia/maia/doctype/gynecology/gynecology.js
+++ maia/maia/doctype/gynecology/gynecology.js
@@ -1,43 +1,71 @@
-// Copyright (c) 2016, DOKOS and contributors
+// Copyright (c) 2018, DOKOS and contributors
 // For license information, please see license.txt
 
-maia.GynecologyController = frappe.ui.form.Controller.extend({
+frappe.ui.form.on('Gynecology', {
 
-    onload: function(frm) {
-	get_first_menses_and_regular_cycles(this.frm);
-    },
+	onload: function(frm) {
+		get_first_menses_and_regular_cycles(frm);
+	},
 
-    refresh: function(frm) {
-	get_first_menses_and_regular_cycles(this.frm);
-    },
+	refresh: function(frm) {
+		get_first_menses_and_regular_cycles(frm);
+	},
+
+	lab_exam_template: function(frm) {
+		if(frm.doc.lab_exam_template) {
+			frappe.call({
+				"method": "maia.maia.doctype.lab_exam_template.lab_exam_template.get_lab_exam_template",
+				args: {
+					lab_exam_template: frm.doc.lab_exam_template
+				},
+				callback: function (data) {
+					frappe.prompt([
+						{'fieldname': 'date', 'fieldtype': 'Date', 'reqd': 1}
+					],
+					function(value){
+						$.each(data.message || [], function(i, v){
+							var d = frappe.model.add_child(frm.doc, "Lab Exam Results", "labs_results");
+							d.date = value.date;
+							d.exam_type = v.exam_type;
+							frappe.db.get_value('Lab Exam Type', {name: d.exam_type}, 'default_value', (r) => {
+									d.show_on_dashboard = r.default_value;
+							});
+						});
+						refresh_field("labs_results");
+					},
+					__('Exams date ?'),
+					__('Validate')
+				);
+				}
+			});
+		}
+	}
 
 });
 
-$.extend(this.frm.cscript, new maia.GynecologyController({frm: this.frm}));
-
 
 var get_first_menses_and_regular_cycles = function(frm) {
-    if (frm.doc.patient_record) {
-	frappe.call({
-	    "method": "frappe.client.get",
-	    args: {
-		doctype: "Patient Record",
-		name: frm.doc.patient_record
-	    },
-	    cache: false,
-	    callback: function (data) {
-		if (data.message) {
-		    first_menses = data.message.first_menses;
-		    regular_cycles = data.message.regular_cycles;
-		    contraception = data.message.contraception;
+	if (frm.doc.patient_record) {
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Patient Record",
+				name: frm.doc.patient_record
+			},
+			cache: false,
+			callback: function(data) {
+				if (data.message) {
+					first_menses = data.message.first_menses;
+					regular_cycles = data.message.regular_cycles;
+					contraception = data.message.contraception;
 
-		 
-		    frappe.model.set_value(frm.doctype, frm.docname, "first_menses", first_menses); 
-		    frappe.model.set_value(frm.doctype, frm.docname, "regular_cycles", regular_cycles);
-		    frappe.model.set_value(frm.doctype, frm.docname, "contraception", contraception);
 
-		}
-	    }
-	})
-    }
+					frappe.model.set_value(frm.doctype, frm.docname, "first_menses", first_menses);
+					frappe.model.set_value(frm.doctype, frm.docname, "regular_cycles", regular_cycles);
+					frappe.model.set_value(frm.doctype, frm.docname, "contraception", contraception);
+
+				}
+			}
+		})
+	}
 };
--- maia/maia/doctype/pregnancy/pregnancy.js
+++ maia/maia/doctype/pregnancy/pregnancy.js
@@ -3,63 +3,92 @@
 frappe.provide("maia");
 
 frappe.ui.form.on('Pregnancy', {
-  onload: function(frm) {
-    frm.trigger("trigger_chart");
-  },
-  refresh: function(frm) {
-    frm.trigger("trigger_chart");
-  },
-  date_time: function(frm) {
-    frm.set_value("birth_datetime", frm.doc.date_time);
-  },
-  trigger_chart: function(frm) {
-    if(frm.doc.birth_weight||frm.doc.release_weight) {
-      frm.events.setup_newborn_chart(frm, 'firstchild', 'weight_curve');
-    }
-    if((frm.doc.twins||frm.doc.triplets)&&(frm.doc.birth_weight_2||frm.doc.release_weight_2)) {
-      frm.events.setup_newborn_chart(frm, 'secondchild', 'weight_curve_2');
-    }
-    if((frm.doc.triplets)&&(frm.doc.birth_weight_3||frm.doc.release_weight_3)) {
-      frm.events.setup_newborn_chart(frm, 'thirdchild', 'weight_curve_3');
-    }
-  },
-  setup_newborn_chart: function(frm, child, domelem) {
+	onload: function(frm) {
+		frm.trigger("trigger_chart");
+	},
+	refresh: function(frm) {
+		frm.trigger("trigger_chart");
+	},
+	date_time: function(frm) {
+		frm.set_value("birth_datetime", frm.doc.date_time);
+	},
+	trigger_chart: function(frm) {
+		if(frm.doc.birth_weight||frm.doc.release_weight) {
+			frm.events.setup_newborn_chart(frm, 'firstchild', 'weight_curve');
+		}
+		if((frm.doc.twins||frm.doc.triplets)&&(frm.doc.birth_weight_2||frm.doc.release_weight_2)) {
+			frm.events.setup_newborn_chart(frm, 'secondchild', 'weight_curve_2');
+		}
+		if((frm.doc.triplets)&&(frm.doc.birth_weight_3||frm.doc.release_weight_3)) {
+			frm.events.setup_newborn_chart(frm, 'thirdchild', 'weight_curve_3');
+		}
+	},
+	setup_newborn_chart: function(frm, child, domelem) {
 
-    frappe.call({
-      method: "maia.maia.doctype.pregnancy.pregnancy.get_newborn_weight_data",
-      args: {
-        patient_record: frm.doc.patient_record,
-        pregnancy: frm.doc.name,
-        child: child
-      },
-      callback: function(r) {
-        if (r.message && r.message[0].datasets[0].values.length !=0) {
-          let data = r.message[0];
-          let colors = r.message[1];
+		frappe.call({
+			method: "maia.maia.doctype.pregnancy.pregnancy.get_newborn_weight_data",
+			args: {
+				patient_record: frm.doc.patient_record,
+				pregnancy: frm.doc.name,
+				child: child
+			},
+			callback: function(r) {
+				if (r.message && r.message[0].datasets[0].values.length !=0) {
+					let data = r.message[0];
+					let colors = r.message[1];
 
-          let $wrap = $('div[data-fieldname='+domelem+']').get(0);
+					let $wrap = $('div[data-fieldname='+domelem+']').get(0);
 
-          let chart = new Chart({
-            parent: $wrap,
-            title: __("Weight Curve (g)"),
-            data: data,
-            type: 'line',
-            height: 150,
-            format_tooltip_y: d => d + ' g',
+					let chart = new Chart({
+						parent: $wrap,
+						title: __("Weight Curve (g)"),
+						data: data,
+						type: 'line',
+						height: 150,
+						format_tooltip_y: d => d + ' g',
 
-            colors: colors,
-          });
-        }
-      }
-    });
-  }
+						colors: colors,
+					});
+				}
+			}
+		});
+	},
+	lab_exam_template: function(frm) {
+		if(frm.doc.lab_exam_template) {
+			frappe.call({
+				"method": "maia.maia.doctype.lab_exam_template.lab_exam_template.get_lab_exam_template",
+				args: {
+					lab_exam_template: frm.doc.lab_exam_template
+				},
+				callback: function (data) {
+					frappe.prompt([
+						{'fieldname': 'date', 'fieldtype': 'Date', 'reqd': 1}
+					],
+					function(value){
+						$.each(data.message || [], function(i, v){
+							var d = frappe.model.add_child(frm.doc, "Lab Exam Results", "labs_results");
+							d.date = value.date;
+							d.exam_type = v.exam_type;
+							frappe.db.get_value('Lab Exam Type', {name: d.exam_type}, 'default_value', (r) => {
+									d.show_on_dashboard = r.default_value;
+							});
+						});
+						refresh_field("labs_results");
+					},
+					__('Exams date ?'),
+					__('Validate')
+				);
+				}
+			});
+		}
+	}
 });
 
 frappe.ui.form.on('Lab Exam Results', {
-  exam_type: function(frm, cdt, cdn) {
-    var row = locals[cdt][cdn];
-    frappe.db.get_value('Lab Exam Type', {name: row.exam_type}, 'default_value', (r) => {
-        frappe.model.set_value(cdt, cdn, "show_on_dashboard", r.default_value);
-    });
-  }
+	exam_type: function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		frappe.db.get_value('Lab Exam Type', {name: row.exam_type}, 'default_value', (r) => {
+				frappe.model.set_value(cdt, cdn, "show_on_dashboard", r.default_value);
+		});
+	}
 });
