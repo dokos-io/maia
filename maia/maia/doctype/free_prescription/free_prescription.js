635a3da118ce5f69a5905a0d5616e7e06bec170c
Charles-Henri Decultot <chdecultot@dokos.io>
2018-03-19 19:08:22 +0000

    Addition exam results template in consultations and prescriptions

--- maia/maia/doctype/free_prescription/free_prescription.js
+++ maia/maia/doctype/free_prescription/free_prescription.js
@@ -4,87 +4,104 @@
 frappe.provide('maia');
 
 frappe.ui.form.on(this.frm.doctype, {
-  onload: function(frm) {
-  if (frm.doc.docstatus != 1) {
-    frappe.call({
-        "method": "maia.client.get_practitioner",
-        args: {
-          doctype: "Professional Information Card",
-          filters: {
-            user: frappe.session.user
-          },
-          fieldname: "name"
-        },
-        cache: false,
-        callback: function(data) {
-          if (!data.exe && data.message) {
-            frappe.model.set_value(frm.doctype, frm.docname, "practitioner", data.message.name)
-          }
-        }
-      })
-    }
-  }
+	onload: function(frm) {
+	if (frm.doc.docstatus != 1) {
+		frappe.call({
+				"method": "maia.client.get_practitioner",
+				args: {
+					doctype: "Professional Information Card",
+					filters: {
+						user: frappe.session.user
+					},
+					fieldname: "name"
+				},
+				cache: false,
+				callback: function(data) {
+					if (!data.exe && data.message) {
+						frappe.model.set_value(frm.doctype, frm.docname, "practitioner", data.message.name)
+					}
+				}
+			})
+		}
+	},
+	lab_exam_template: function(frm) {
+		if(frm.doc.lab_exam_template) {
+			frappe.call({
+				"method": "maia.maia.doctype.lab_exam_template.lab_exam_template.get_lab_exam_template",
+				args: {
+					lab_exam_template: frm.doc.lab_exam_template
+				},
+				callback: function (data) {
+						$.each(data.message || [], function(i, v){
+							var d = frappe.model.add_child(frm.doc, "Lab Exam Prescription", "lab_prescription_table");
+							d.lab_exam = v.exam_type;
+						});
+						refresh_field("lab_prescription_table");
+				}
+			});
+		}
+	}
 
 });
 
 maia.FreePrescriptionController = frappe.ui.form.Controller.extend({
 
-  refresh: function(frm) {
-    if (!this.frm.doc.__islocal) {
-      this.frm.add_custom_button(__('Drug Prescription'), this.print_drug_prescription, __("Print Prescription"));
-      this.frm.add_custom_button(__('Lab Prescription'), this.print_lab_prescription, __("Print Prescription"));
-      this.frm.add_custom_button(__('Echography Prescription'), this.print_echo_prescription, __("Print Prescription"));
-    }
+	refresh: function(frm) {
+		if (!this.frm.doc.__islocal) {
+			this.frm.add_custom_button(__('Drug Prescription'), this.print_drug_prescription, __("Print Prescription"));
+			this.frm.add_custom_button(__('Lab Prescription'), this.print_lab_prescription, __("Print Prescription"));
+			this.frm.add_custom_button(__('Echography Prescription'), this.print_echo_prescription, __("Print Prescription"));
+		}
 
-  },
+	},
 
-  print_drug_prescription: function(frm) {
-    var w = window.open(
-      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
-        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
-        "&name=" + encodeURIComponent(cur_frm.doc.name) +
-        "&format=Drug Prescription" +
-        "&no_letterhead=0" +
-        "&_lang=fr")
-    );
-    if (!w) {
-      frappe.msgprint(__("Please enable pop-ups"));
-      return;
-    }
-  },
+	print_drug_prescription: function(frm) {
+		var w = window.open(
+			frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+				"doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+				"&name=" + encodeURIComponent(cur_frm.doc.name) +
+				"&format=Drug Prescription" +
+				"&no_letterhead=0" +
+				"&_lang=fr")
+		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
+	},
 
-  print_lab_prescription: function(frm) {
-    var w = window.open(
-      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
-        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
-        "&name=" + encodeURIComponent(cur_frm.doc.name) +
-        "&format=Lab Prescription" +
-        "&no_letterhead=0" +
-        "&_lang=fr")
-    );
-    if (!w) {
-      frappe.msgprint(__("Please enable pop-ups"));
-      return;
-    }
-  },
+	print_lab_prescription: function(frm) {
+		var w = window.open(
+			frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+				"doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+				"&name=" + encodeURIComponent(cur_frm.doc.name) +
+				"&format=Lab Prescription" +
+				"&no_letterhead=0" +
+				"&_lang=fr")
+		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
+	},
 
-  print_echo_prescription: function(frm) {
-    var w = window.open(
-      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
-        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
-        "&name=" + encodeURIComponent(cur_frm.doc.name) +
-        "&format=Echography Prescription" +
-        "&no_letterhead=0" +
-        "&_lang=fr")
-    );
-    if (!w) {
-      frappe.msgprint(__("Please enable pop-ups"));
-      return;
-    }
-  }
+	print_echo_prescription: function(frm) {
+		var w = window.open(
+			frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+				"doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+				"&name=" + encodeURIComponent(cur_frm.doc.name) +
+				"&format=Echography Prescription" +
+				"&no_letterhead=0" +
+				"&_lang=fr")
+		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
+	}
 
 });
 
 $.extend(cur_frm.cscript, new maia.FreePrescriptionController({
-  frm: cur_frm
+	frm: cur_frm
 }));
--- maia/maia/doctype/pregnancy_consultation/pregnancy_consultation.js
+++ maia/maia/doctype/pregnancy_consultation/pregnancy_consultation.js
@@ -5,154 +5,154 @@ frappe.provide("maia");
 {% include "maia/public/js/controllers/consultations.js" %}
 
 frappe.ui.form.on('Pregnancy Consultation', {
-   weight: function(frm) {
-     if (frm.doc.weight && frm.doc.patient_record) {
-       frappe.call({
-         method: "maia.maia.doctype.pregnancy_consultation.pregnancy_consultation.get_base_weight",
-         args: {
-           patient_record: frm.doc.patient_record
-         },
-         callback: function(r) {
-           if (r.message) {
-             let daily_weight = frm.doc.weight;
-             let difference = daily_weight - r.message;
-             frappe.model.set_value('Pregnancy Consultation', frm.doc.name, 'weight_gain', difference)
-           }
-         }
-       })
-     }
-   },
-   pregnancy_folder: function(frm) {
-     get_term_date(frm);
-   }
+	 weight: function(frm) {
+		 if (frm.doc.weight && frm.doc.patient_record) {
+			 frappe.call({
+				 method: "maia.maia.doctype.pregnancy_consultation.pregnancy_consultation.get_base_weight",
+				 args: {
+					 patient_record: frm.doc.patient_record
+				 },
+				 callback: function(r) {
+					 if (r.message) {
+						 let daily_weight = frm.doc.weight;
+						 let difference = daily_weight - r.message;
+						 frappe.model.set_value('Pregnancy Consultation', frm.doc.name, 'weight_gain', difference)
+					 }
+				 }
+			 })
+		 }
+	 },
+	 pregnancy_folder: function(frm) {
+		 get_term_date(frm);
+	 }
 })
 
 maia.PregnancyConsultationController = frappe.ui.form.Controller.extend({
 
-  onload: function(frm) {
-    if(this.frm.doc.docstatus!=1) {
-      get_term_date(this.frm);
-    }
+	onload: function(frm) {
+		if(this.frm.doc.docstatus!=1) {
+			get_term_date(this.frm);
+		}
 
-    this.frm.fields_dict['pregnancy_folder'].get_query = function(doc) {
-      return {
-        filters: {
-          "patient_record": doc.patient_record
-        }
-      }
-    }
+		this.frm.fields_dict['pregnancy_folder'].get_query = function(doc) {
+			return {
+				filters: {
+					"patient_record": doc.patient_record
+				}
+			}
+		}
 
-  },
-  consultation_date: function(frm) {
-    get_term_date(this.frm);
-  },
-  refresh: function(frm) {
-    if (!this.frm.doc.__islocal) {
-      this.frm.add_custom_button(__('Drug Prescription'), this.print_drug_prescription, __("Print Prescription"));
-      this.frm.add_custom_button(__('Lab Prescription'), this.print_lab_prescription, __("Print Prescription"));
-      this.frm.add_custom_button(__('Echography Prescription'), this.print_echo_prescription, __("Print Prescription"));
-    }
-    if(this.frm.doc.docstatus!=1) {
-     get_term_date(this.frm)
-   }
-  },
+	},
+	consultation_date: function(frm) {
+		get_term_date(this.frm);
+	},
+	refresh: function(frm) {
+		if (!this.frm.doc.__islocal) {
+			this.frm.add_custom_button(__('Drug Prescription'), this.print_drug_prescription, __("Print Prescription"));
+			this.frm.add_custom_button(__('Lab Prescription'), this.print_lab_prescription, __("Print Prescription"));
+			this.frm.add_custom_button(__('Echography Prescription'), this.print_echo_prescription, __("Print Prescription"));
+		}
+		if(this.frm.doc.docstatus!=1) {
+		 get_term_date(this.frm)
+	 }
+	},
 
-  print_drug_prescription: function(frm) {
-    var w = window.open(
-      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
-        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
-        "&name=" + encodeURIComponent(cur_frm.doc.name) +
-        "&format=Drug Prescription" +
-        "&no_letterhead=0" +
-        "&_lang=fr")
-    );
-    if (!w) {
-      frappe.msgprint(__("Please enable pop-ups"));
-      return;
-    }
-  },
+	print_drug_prescription: function(frm) {
+		var w = window.open(
+			frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+				"doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+				"&name=" + encodeURIComponent(cur_frm.doc.name) +
+				"&format=Drug Prescription" +
+				"&no_letterhead=0" +
+				"&_lang=fr")
+		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
+	},
 
-  print_lab_prescription: function(frm) {
-    var w = window.open(
-      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
-        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
-        "&name=" + encodeURIComponent(cur_frm.doc.name) +
-        "&format=Lab Prescription" +
-        "&no_letterhead=0" +
-        "&_lang=fr")
-    );
-    if (!w) {
-      frappe.msgprint(__("Please enable pop-ups"));
-      return;
-    }
-  },
+	print_lab_prescription: function(frm) {
+		var w = window.open(
+			frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+				"doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+				"&name=" + encodeURIComponent(cur_frm.doc.name) +
+				"&format=Lab Prescription" +
+				"&no_letterhead=0" +
+				"&_lang=fr")
+		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
+	},
 
-  print_echo_prescription: function(frm) {
-    var w = window.open(
-      frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
-        "doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
-        "&name=" + encodeURIComponent(cur_frm.doc.name) +
-        "&format=Echography Prescription" +
-        "&no_letterhead=0" +
-        "&_lang=fr")
-    );
-    if (!w) {
-      frappe.msgprint(__("Please enable pop-ups"));
-      return;
-    }
-  },
+	print_echo_prescription: function(frm) {
+		var w = window.open(
+			frappe.urllib.get_full_url("/api/method/frappe.utils.print_format.download_pdf?" +
+				"doctype=" + encodeURIComponent(cur_frm.doc.doctype) +
+				"&name=" + encodeURIComponent(cur_frm.doc.name) +
+				"&format=Echography Prescription" +
+				"&no_letterhead=0" +
+				"&_lang=fr")
+		);
+		if (!w) {
+			frappe.msgprint(__("Please enable pop-ups"));
+			return;
+		}
+	},
 
-  set_default_print_format: function() {
-    this.frm.meta.default_print_format = "";
-  }
+	set_default_print_format: function() {
+		this.frm.meta.default_print_format = "";
+	}
 
 });
 $.extend(this.frm.cscript, new maia.PregnancyConsultationController({
-  frm: this.frm
+	frm: this.frm
 }));
 
 
 var get_term_date = function(frm) {
-  if (frm.doc.pregnancy_folder) {
-    frappe.call({
-      "method": "frappe.client.get",
-      args: {
-        doctype: "Pregnancy",
-        name: frm.doc.pregnancy_folder
-      },
-      cache: false,
-      callback: function(data) {
-        if (data.message) {
-          expected_term = data.message.expected_term;
-          beginning_of_pregnancy = data.message.beginning_of_pregnancy;
-          last_menstrual_period = data.message.last_menstrual_period;
-          consultation_date = frm.doc.consultation_date;
+	if (frm.doc.pregnancy_folder) {
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Pregnancy",
+				name: frm.doc.pregnancy_folder
+			},
+			cache: false,
+			callback: function(data) {
+				if (data.message) {
+					expected_term = data.message.expected_term;
+					beginning_of_pregnancy = data.message.beginning_of_pregnancy;
+					last_menstrual_period = data.message.last_menstrual_period;
+					consultation_date = frm.doc.consultation_date;
 
-          if (expected_term != null) {
+					if (expected_term != null) {
 
-            am_weeks = Math.floor((275 - frappe.datetime.get_diff(expected_term, consultation_date)) / 7) + 2
-            add_days = Math.floor(((275 - frappe.datetime.get_diff(expected_term, consultation_date)) / 7 - (am_weeks - 2)) * 7)
+						am_weeks = Math.floor((275 - frappe.datetime.get_diff(expected_term, consultation_date)) / 7) + 2
+						add_days = Math.floor(((275 - frappe.datetime.get_diff(expected_term, consultation_date)) / 7 - (am_weeks - 2)) * 7)
 
-            frappe.model.set_value(frm.doctype, frm.docname, "term", am_weeks + " " + __("Weeks Amenorrhea +") + " " + add_days + " " + __("Days"))
-          } else if (beginning_of_pregnancy != null) {
-            am_weeks = Math.floor(frappe.datetime.get_diff(consultation_date, beginning_of_pregnancy) / 7) + 2
-            add_days = Math.floor((frappe.datetime.get_diff(consultation_date, beginning_of_pregnancy) / 7 - Math.floor(frappe.datetime.get_diff(consultation_date, beginning_of_pregnancy) / 7)) * 7)
+						frappe.model.set_value(frm.doctype, frm.docname, "term", am_weeks + " " + __("Weeks Amenorrhea +") + " " + add_days + " " + __("Days"))
+					} else if (beginning_of_pregnancy != null) {
+						am_weeks = Math.floor(frappe.datetime.get_diff(consultation_date, beginning_of_pregnancy) / 7) + 2
+						add_days = Math.floor((frappe.datetime.get_diff(consultation_date, beginning_of_pregnancy) / 7 - Math.floor(frappe.datetime.get_diff(consultation_date, beginning_of_pregnancy) / 7)) * 7)
 
-            frappe.model.set_value(frm.doctype, frm.docname, "term", am_weeks + " " + __("Weeks Amenorrhea +") + " " + add_days + " " + __("Days"))
-          } else if (last_menstrual_period != null) {
-            am_weeks = Math.floor(frappe.datetime.get_diff(consultation_date, last_menstrual_period) / 7)
-            add_days = Math.floor((frappe.datetime.get_diff(consultation_date, last_menstrual_period) / 7 - Math.floor(frappe.datetime.get_diff(consultation_date, last_menstrual_period) / 7)) * 7)
+						frappe.model.set_value(frm.doctype, frm.docname, "term", am_weeks + " " + __("Weeks Amenorrhea +") + " " + add_days + " " + __("Days"))
+					} else if (last_menstrual_period != null) {
+						am_weeks = Math.floor(frappe.datetime.get_diff(consultation_date, last_menstrual_period) / 7)
+						add_days = Math.floor((frappe.datetime.get_diff(consultation_date, last_menstrual_period) / 7 - Math.floor(frappe.datetime.get_diff(consultation_date, last_menstrual_period) / 7)) * 7)
 
-            frappe.model.set_value(frm.doctype, frm.docname, "term", am_weeks + " " + __("Weeks Amenorrhea +") + " " + add_days + " " + __("Days"))
+						frappe.model.set_value(frm.doctype, frm.docname, "term", am_weeks + " " + __("Weeks Amenorrhea +") + " " + add_days + " " + __("Days"))
 
-          } else {
+					} else {
 
-            frappe.model.set_value(frm.doctype, frm.docname, "term", "");
-          }
-        }
-      }
-    })
-  } else {
-    frappe.model.set_value(frm.doctype, frm.docname, "day", __("Please Set an Expected Term or Beginning of Pregnancy Date in the Pregnancy Folder"))
-  }
+						frappe.model.set_value(frm.doctype, frm.docname, "term", "");
+					}
+				}
+			}
+		})
+	} else {
+		frappe.model.set_value(frm.doctype, frm.docname, "day", __("Please Set an Expected Term or Beginning of Pregnancy Date in the Pregnancy Folder"))
+	}
 };
