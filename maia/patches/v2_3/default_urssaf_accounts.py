cbf0d39c5f45c10b2a1ca605a6a9e866f87eaf39
Charles-Henri Decultot <chdecultot@dokos.io>
2018-04-02 11:07:45 +0000

    Patch correction

--- maia/patches/v2_3/default_urssaf_accounts.py
+++ maia/patches/v2_3/default_urssaf_accounts.py
@@ -11,15 +11,13 @@ def execute():
 
 	for company in companies:
 		abbr = frappe.get_value("Company", company.name, "abbr")
-		social_contribution_deductible_account = "645 - Charges de sécurité sociale et de prévoyance - " + args.get('company_abbr')
+		social_contribution_deductible_account = "645 - Charges de sécurité sociale et de prévoyance - " + abbr
 		social_contributions_third_party = "URSSAF"
 		try:
 			if frappe.db.exists('Account', social_contribution_deductible_account):
-				frappe.db.set_value('Company', args.get(
-					'company_name'), 'social_contribution_deductible_account', social_contribution_deductible_account)
+				frappe.db.set_value('Company', company.name, 'social_contribution_deductible_account', social_contribution_deductible_account)
 
 			if frappe.db.exists('Supplier', social_contributions_third_party):
-				frappe.db.set_value('Company', args.get(
-					'company_name'), 'social_contributions_third_party', social_contributions_third_party)
+				frappe.db.set_value('Company', company.name, 'social_contributions_third_party', social_contributions_third_party)
 		except Exception as e:
 			print(e)
