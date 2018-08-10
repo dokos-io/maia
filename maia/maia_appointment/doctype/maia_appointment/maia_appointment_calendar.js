3e0cb9c95df9beab50818cf7f613bb307d108c7b
Charles-Henri Decultot <chdecultot@dokos.io>
2018-08-10 16:34:38 +0000

    Zones grisées dans l'agenda + correction bilans

--- maia/maia_appointment/doctype/maia_appointment/maia_appointment_calendar.js
+++ maia/maia_appointment/doctype/maia_appointment/maia_appointment_calendar.js
@@ -1,26 +1,54 @@
+var get_business_hours = function() {
+	let mapping = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7}
+	let agenda = [];
+	frappe.call({
+		async: false,
+		method: "frappe.client.get",
+		args: {doctype: "Professional Information Card", filters: {user: frappe.session.user}},
+		callback: function (data) {
+			if (data.message.consulting_schedule.length>0) {
+				data.message.consulting_schedule.forEach(value => {
+					agenda.push({dow: [mapping[value.day]], start: value.start_time, end: value.end_time})
+				})
+				return agenda;
+			} else {
+				return agenda = false;
+			}
+		}
+	});
+	
+	return (agenda)
+}
+
 frappe.views.calendar["Maia Appointment"] = {
-    field_map: {
-	"start": "start_dt",
-	"end": "end_dt",
-	"id": "name",
-	"title": "subject",
-	"allDay": "allDay",
-	"color": "color"
-    },
-    gantt: false,
-    get_events_method: "maia.maia_appointment.doctype.maia_appointment.maia_appointment.get_events",
-    filters: [
-	{
-	    'fieldtype': 'Link',
-	    'fieldname': 'patient_record',
-	    'options': 'Patient Record',
-	    'label': __('Patient')
+	field_map: {
+		"start": "start_dt",
+		"end": "end_dt",
+		"id": "name",
+		"title": "subject",
+		"allDay": "allDay",
+		"color": "color"
 	},
-	{
-	    'fieldtype': 'Link',
-	    'fieldname': 'appointment_type',
-	    'options': 'Maia Appointment Type',
-	    'label': __('Appointment Type')
+	gantt: false,
+	get_events_method: "maia.maia_appointment.doctype.maia_appointment.maia_appointment.get_events",
+	filters: [
+		{
+			'fieldtype': 'Link',
+			'fieldname': 'patient_record',
+			'options': 'Patient Record',
+			'label': __('Patient')
+		},
+		{
+			'fieldtype': 'Link',
+			'fieldname': 'appointment_type',
+			'options': 'Maia Appointment Type',
+			'label': __('Appointment Type')
+		}
+	],
+	options: {
+		businessHours: get_business_hours(),
+		minTime: '06:00:00',
+		maxTime: '22:00:00',
+		scrollTime: '08:00:00'
 	}
-    ]
 }
