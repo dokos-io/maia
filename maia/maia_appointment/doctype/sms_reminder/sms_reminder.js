4b47ad85786e0fb9f8c5ad84e997f2ac38b4c604
Charles-Henri Decultot <chdecultot@dokos.io>
2019-06-16 20:15:23 +0200

    fix: post prod corrections

--- maia/maia_appointment/doctype/sms_reminder/sms_reminder.js
+++ maia/maia_appointment/doctype/sms_reminder/sms_reminder.js
@@ -3,6 +3,19 @@
 
 frappe.ui.form.on('SMS Reminder', {
 	refresh: function(frm) {
-
+		edit_values(frm, false);
+		frm.add_custom_button(__('Edit values'), function() {
+			edit_values(frm, true);
+		});
 	}
 });
+
+const edit_values = (frm, enable) => {
+	console.log(enable)
+	if (frm.doc.status != "Sent") {
+		const fields = ["sender_name", "send_on", "send_to", "message"]
+		fields.forEach(f => {
+			frm.toggle_enable(f, enable);
+		})
+	}
+}
