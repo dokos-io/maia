c076268bba70e106091da33ca88b4efc3e7b4ace
Charles-Henri Decultot <chdecultot@dokos.io>
2019-06-17 23:47:27 +0200

    fix: print format patch

--- maia/patches/v3_0/default_prenatal_consult_formats.py
+++ maia/patches/v3_0/default_prenatal_consult_formats.py
@@ -3,4 +3,5 @@ import frappe
 from frappe.printing.doctype.print_format.print_format import make_default
 
 def execute():
+	frappe.reload_doc('maia', 'print_format', 'prenatal_interview_consultation')
 	make_default("Prenatal Interview Consultation")
