f528cfacbe0ce8c37f24fddf60fa83bc86e5cf0f
Charles-Henri Decultot <chdecultot@dokos.io>
2019-06-18 17:07:17 +0200

    v3.0.3

--- maia/patches/v3_0/missing_default_values.py
+++ maia/patches/v3_0/missing_default_values.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 import frappe
+from maia.setup.setup_wizard.operations.maia_setup import set_default_print_formats
 
 def execute():
 	pics = frappe.get_all("Professional Information Card")
 	for pic in pics:
 		frappe.db.set_value("Professional Information Card", pic.name, "social_security_rate", "Normal Rate (70%)")
+
+	set_default_print_formats()
